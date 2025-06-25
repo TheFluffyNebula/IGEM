e1f84aa8509d6182287fe08d0fde3abfd34c3650
TheFluffyNebula <jasonyin30@gmail.com>
2025-06-25 16:41:38 -0400

    main file to run configs

--- new_src/run_benchmarks.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-nohup python3 -u ./src/run_experiments.py > benchmark.log 2>&1 &
-disown
--- new_src/verify_running.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-ps aux | grep run_experiments.py 
-tail -f benchmark.log
