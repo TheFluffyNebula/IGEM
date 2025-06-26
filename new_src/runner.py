99b6a670c3125aa5d6761bab0f35b7ad9dc7f047
TheFluffyNebula <jasonyin30@gmail.com>
2025-06-26 11:18:55 -0400

    main.py refactoring (yaml)
    Co-authored-by: etekmen13 <etekmen13@users.noreply.github.com>

--- new_src/runner.py
+++ new_src/runner.py
@@ -1,46 +1,11 @@
-# benchmark each command
-DEFAULT_ARGS = {
-    "seed": 0,
-    "train_mb_size": 32,      
-    "eval_mb_size": 1000,
-    "n_experiences": 10,      
-    "train_epochs": 1,    
-    "lr": 0.1,               
-    "momentum": 0.0,
-    "patterns_per_exp": 256,   
-    "memory_strength": 0.5,
-    "output_dir": "./eval_results",
-    "cuda": 0,
+class Runner:
+    # explicitly pass all keywords (replace strategy_keywords)
+    def __init__(self, benchmark, strategy, strategy_keywords):
+        self.benchmark = benchmark
+        self.strategy = strategy
+        self.strategy_keywords = strategy_keywords
     
-    # GEM
-    "memory_size" : 10, 
-    # IGEM
-    "sgd_iterations": 3,
-    # AGEM
-    "sample_size" : -1, # AGEM placeholder,
-    # N_ITER
-    "projection_iteration": [1, 25, 100, 500],
-}
-DEBUG_ARGS = {
-    "seed": 0,
-    "train_mb_size": 32,
-    "eval_mb_size": 1000,
-    "n_experiences": 3,
-    "train_epochs": 1,
-    "lr": 0.01,
-    "momentum": 0.0,
-    "patterns_per_exp": 10,
-    "memory_strength": 0.5,
-    "output_dir": "./new_src/debug_results/minitest",
-    "cuda" : 0,
-    
-    # N_ITER
-    "projection_iteration": [2, 100],
-    # GEM
-    "memory_size" : 10, 
-    #AGEM
-    "sample_size" : -1,
-}
+
 
 '''
 example runs (file + options)
